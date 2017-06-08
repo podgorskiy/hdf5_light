92e2f9729df29741a2e5e7dbb64b40bf56287560
Podgorskiy <stanislav@podgorskiy.com>
2017-06-08 14:42:31 -0400

    HDF5 version 1.10.1

---
+++ src/H5pubconf.h
@@ -0,0 +1,72 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the root of the source code       *
+ * distribution tree, or in https://support.hdfgroup.org/ftp/HDF5/releases.  *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef H5_CONFIG_H_
+#define H5_CONFIG_H_
+
+#define H5_HAVE_WINDOWS
+#define H5_HAVE_WIN32_API
+#define H5_HAVE_WINSOCK2_H
+#define H5_HAVE_VISUAL_STUDIO
+#define H5_CXX_HAVE_OFFSETOF
+#define H5_HAVE_INTTYPES_H
+#define H5_STDC_HEADERS
+#define H5_HAVE_STDINT_H
+#define H5_HAVE_STDINT_H_CXX
+#define H5_HAVE_STDLIB_H
+#define H5_HAVE_STDDEF_H
+#define H5_HAVE_STDBOOL_H
+#define H5_HAVE_INLINE
+#define H5_HAVE_ZLIB_H
+#define H5_HAVE_SYS_TYPES_H
+#define H5_HAVE_SYS_STAT_H
+#define H5_HAVE_IO_H
+#define H5_HAVE_GETHOSTNAME
+
+#ifdef LLP64
+#define H5_SIZEOF_LONG 4
+#elif LP64
+#define H5_SIZEOF_LONG 8
+#endif
+
+#define H5_SIZEOF_LONG_LONG 8
+#define H5_SIZEOF_BOOL 1
+#define H5_SIZEOF_CHAR 1
+#define H5_SIZEOF_DOUBLE 8
+#define H5_SIZEOF_FLOAT 4
+#define H5_SIZEOF_INT 4
+#define H5_SIZEOF_INT8_T 1
+#define H5_SIZEOF_INT16_T 2
+#define H5_SIZEOF_INT32_T 4
+#define H5_SIZEOF_INT64_T 8
+#define H5_SIZEOF_UINT 4
+#define H5_SIZEOF_UINT8_T 1
+#define H5_SIZEOF_UINT16_T 2
+#define H5_SIZEOF_UINT32_T 4
+#define H5_SIZEOF_UINT64_T 8
+
+#define H5_SIZEOF_LONG_DOUBLE 8
+#define H5_SIZEOF_OFF64_T 8
+#define H5_SIZEOF_OFF_T 4
+#define H5_SIZEOF_PTRDIFF_T 8
+#define H5_SIZEOF_SHORT 2
+#define H5_SIZEOF___INT64 8
+
+#define H5_SIZEOF_SIZE_T 8
+
+#define H5_HAVE_FUNCTION
+
+#define H5_PRINTF_LL_WIDTH "lld"
+
+#define H5_DEFAULT_PLUGINDIR "%ALLUSERSPROFILE%\\\\hdf5\\\\lib\\\\plugin"
+
+#endif
