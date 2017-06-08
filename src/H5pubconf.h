ac5cd7dabfa7904035d703dcd0731b3ffdfa025c
Podgorskiy <stanislav@podgorskiy.com>
2017-06-08 15:24:15 -0400

    Windows 32bit and 64bit configuration + Linux and OS X

--- src/H5pubconf.h
+++ src/H5pubconf.h
@@ -13,10 +13,42 @@
 #ifndef H5_CONFIG_H_
 #define H5_CONFIG_H_
 
+#ifdef _H5private_H
+#pragma warning(disable: 4996)
+#endif
+
+#include <stdint.h>
+#if INTPTR_MAX == INT64_MAX
+#define H5_SIZEOF_SIZE_T 8
+#define H5_SIZEOF_PTRDIFF_T 8
+#elif INTPTR_MAX == INT32_MAX
+#define H5_SIZEOF_SIZE_T 4
+#define H5_SIZEOF_PTRDIFF_T 4
+#else
+#error Unknown pointer size or missing size macros!
+#endif
+
+#define H5_HAVE_THREADSAFE
+
+#if defined _WIN32
 #define H5_HAVE_WINDOWS
 #define H5_HAVE_WIN32_API
 #define H5_HAVE_WINSOCK2_H
 #define H5_HAVE_VISUAL_STUDIO
+#define H5_HAVE_WIN_THREADS
+#else
+#define H5_HAVE_PTHREAD_H
+#define H5_HAVE_FLOCK
+#define H5_HAVE_UNISTD_H
+#define H5_TIME_WITH_SYS_TIME
+#define H5_HAVE_SYS_FILE_H
+#define H5_HAVE_SYS_RESOURCE_H
+#define H5_HAVE_SYS_IOCTL_H
+#define H5_HAVE_SYS_SYSINFO_H
+#define H5_HAVE_SYS_PROC_H
+#define H5_HAVE_SYS_PROC_H
+#endif
+
 #define H5_CXX_HAVE_OFFSETOF
 #define H5_HAVE_INTTYPES_H
 #define H5_STDC_HEADERS
@@ -29,12 +61,13 @@
 #define H5_HAVE_ZLIB_H
 #define H5_HAVE_SYS_TYPES_H
 #define H5_HAVE_SYS_STAT_H
+#define H5_HAVE_STRDUP
 #define H5_HAVE_IO_H
 #define H5_HAVE_GETHOSTNAME
 
-#ifdef LLP64
+#if defined _WIN32
 #define H5_SIZEOF_LONG 4
-#elif LP64
+#else
 #define H5_SIZEOF_LONG 8
 #endif
 
@@ -57,11 +90,8 @@
 #define H5_SIZEOF_LONG_DOUBLE 8
 #define H5_SIZEOF_OFF64_T 8
 #define H5_SIZEOF_OFF_T 4
-#define H5_SIZEOF_PTRDIFF_T 8
 #define H5_SIZEOF_SHORT 2
 #define H5_SIZEOF___INT64 8
-
-#define H5_SIZEOF_SIZE_T 8
 
 #define H5_HAVE_FUNCTION
 
